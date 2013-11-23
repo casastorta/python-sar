e16d9f3b0ebe9931afc89d7551f795d055700942
Vedran Krivokuca <vedran.krivokuca@nimium.hr>
2013-11-23 17:06:42 +0100

    Bump to 0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='sar',
-      version='0.2',
+      version='0.2.1',
       description='SAR (sysstat) output files parser',
       author='Vedran Krivokuca',
       author_email='dev@krivokuca.net',
